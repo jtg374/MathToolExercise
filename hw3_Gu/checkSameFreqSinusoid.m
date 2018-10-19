3594b910b3143163f1446caf9b96a3aea3e166db
jtg374 <jtg374@nyu.edu>
2018-10-19 12:51:51 -0400

    hw3

---
+++ hw3_Gu/checkSameFreqSinusoid.m
@@ -0,0 +1,15 @@
+function phaseDiff=checkSameFreqSinusoid(f,p,system,fig)
+    t = (1:64)';
+    input = cos(f*t+p)+1;
+    output = system(input);
+    output_ = output-mean(output); % substract mean
+    output_n = output_/max(abs(output_)); % and normalize
+    if fig
+        figure; hold on
+        plot(input)
+        plot(output_n+1)
+        legend('input','normalized output')
+        xlabel('time')
+    end
+    phaseDiff = acos(dot(input,output)/(norm(input)*norm(output)));
+    
--- hw3_Gu/createConvMat.m
+++ hw3_Gu/createConvMat.m
@@ -2,5 +2,5 @@ function X=createConvMat(x,M)
     % size(x) should be [N,1]
     xPad = [x;zeros(M-1,1)];
     for ii=1:M
-        X(:,ii) = circshift(xPad,M-1);
+        X(:,ii) = circshift(xPad,ii-1);
     end
